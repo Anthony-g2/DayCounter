fe65a2e8539ce08ad75214afe3e80ac4ba0f17a2
Anthony-g2 <ant.greenwell2@gmail.com>
2018-07-17 12:12:26 -0700

    footer

--- app.js
+++ app.js
@@ -8,24 +8,37 @@ $(document).ready(function(){
   });
 
   $('#submit').click(function(){
-    let date1 = $('#datepicker').val();
-    let date2 = $('#datepicker2').val();
+    const date1 = $('#datepicker').val();
+    const date2 = $('#datepicker2').val();
+    const numberWithCommas = (x) => {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
     if (!date1 || !date2){
-      console.log('Please enter two different days!')
+      $('#errormsg').text('Please enter two dates!');
+      $('#errormsg').fadeIn("fast", function() {
+        $(this).delay(1000).fadeOut("slow");
+      });
     } else {
-      console.log(date1,date2)
       let now = moment(date1,'MM-DD-YYYY');
       let expiration = moment(date2,'MM-DD-YYYY');
       let diff = expiration.diff(now,'days');
       let diffH = expiration.diff(now,'hours');
       let diffM = expiration.diff(now,'minutes');
-      if (diff == 1) {
-        $('#results').html('1 Day!')
-        console.log(diff,diffH,diffM,diffS)
+      if (diff < 0) {
+        $('#errormsg').text('First date must be before the second date!');
+        $('#errormsg').fadeIn("fast", function() {
+          $(this).delay(1000).fadeOut("slow");
+        });
+      } else if (isNaN(diff)) {
+        $('#errormsg').text('Dates must be valid!');
+        $('#errormsg').fadeIn("fast", function() {
+          $(this).delay(1000).fadeOut("slow");
+        });
       } else {
-        $('#results-d').html(diff);
-        $('#results-h').html(diffH);
-        $('#results-m').html(diffM);
+        $('#results-d').html(numberWithCommas(diff));
+        $('#results-h').html(numberWithCommas(diffH));
+        $('#results-m').html(numberWithCommas(diffM));
       }
     }
   });
