af690d3317d5f10051870664ca98556114cb327c
Anthony-g2 <ant.greenwell2@gmail.com>
2018-07-17 10:47:35 -0700

    momentjs

--- app.js
+++ app.js
@@ -7,4 +7,27 @@ $(document).ready(function(){
     uiLibrary: 'bootstrap4'
   });
 
+  $('#submit').click(function(){
+    let date1 = $('#datepicker').val();
+    let date2 = $('#datepicker2').val();
+    if (!date1 || !date2){
+      console.log('Please enter two different days!')
+    } else {
+      console.log(date1,date2)
+      let now = moment(date1,'MM-DD-YYYY');
+      let expiration = moment(date2,'MM-DD-YYYY');
+      let diff = expiration.diff(now,'days');
+      let diffH = expiration.diff(now,'hours');
+      let diffM = expiration.diff(now,'minutes');
+      if (diff == 1) {
+        $('#results').html('1 Day!')
+        console.log(diff,diffH,diffM,diffS)
+      } else {
+        $('#results-d').html(diff);
+        $('#results-h').html(diffH);
+        $('#results-m').html(diffM);
+      }
+    }
+  });
+
 });
